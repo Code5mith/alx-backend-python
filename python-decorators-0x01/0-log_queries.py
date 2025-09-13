561af192440b716021c986d009958fb85f71d077
Code5mith <jacobscontact1@gmail.com>
2025-09-13 15:03:58 -0700

    Fix : add time log

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,16 +1,17 @@
 import sqlite3
 import functools
+from datetime import date
 
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         if args:
             query = args[0]
-            print(f"Executing SQL query: '{query}'")
+            print(f"Executing SQL query: '{query}' {date.today()}")
         else:
             query = kwargs.get('query')
             if query:
-                print(f"Executing SQL query: '{query}'")
+                print(f"Executing SQL query: '{date.today()}'")
 
         results = func(*args, **kwargs)
         return results
@@ -25,3 +26,8 @@ def fetch_all_users(query):
     results = cursor.fetchall()
     conn.close()
     return results
+
+
+users = fetch_all_users(query="SELECT * FROM users")
+print("\nQuery results:")
+print(users)

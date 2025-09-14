536ff14e1da7528950fad16b9974da8b98261929
Code5mith <jacobscontact1@gmail.com>
2025-09-13 18:24:26 -0700

    Task : Reusable Query Context Manager

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -1,16 +1,17 @@
 import sqlite3
 
 class ExecuteQuery():
-    def __init__(self, db_name, age):
+    def __init__(self, db_name, query, params=()):
         self.db_name = db_name
-        self.age = age 
+        self.query = query
+        self.params = params
 
     def __enter__(self):
         conn = None
         try:
             conn = sqlite3.connect(self.db_name)
             cursor = conn.cursor()
-            cursor.execute(f"SELECT * FROM users WHERE age > {self.age}")
+            cursor.execute(self.query, self.params)
             results = cursor.fetchall()
             return results
         except sqlite3.Error as e:
@@ -21,5 +22,16 @@ class ExecuteQuery():
                 conn.close()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        # connection is cleaned already
+        # connection already cleaned
         pass
+
+
+with ExecuteQuery(
+        db_name=':memory:',
+        query="SELECT name, age FROM users WHERE age > ?",
+        params=(25,)
+    ) as results:
+        if results:
+            print("Query results:", results)
+        else:
+            print("No results returned.")

0bd0bc50ed8500e065921f375d73048f1e2d1d7b
Code5mith <jacobscontact1@gmail.com>
2025-09-21 19:01:08 -0700

    Refactor : remote test 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,16 +8,6 @@ class TestAccessNestedMap(unittest.TestCase):
         super().__init__(methodName)
 
     @parameterized.expand([
-    # Test case 1: simple key
-    ({"a": 1}, ("a",), 1),
-    # Test case 2: nested dictionary
-    ({"a": {"b": 2}}, ("a",), {"b": 2}),
-    # Test case 3: two-level nested key
-    ({"a": {"b": 2}}, ("a", "b"), 2),])
-    def test_access_nested_map(self, nested_map, path, expected):
-        self.assertEqual(access_nested_map(nested_map, path), expected)
-
-    @parameterized.expand([
     ({}, ("a",)),
     ({"a":1}, ("a","b")),])
     def test_access_nested_map_exception(self, nested_map, path):

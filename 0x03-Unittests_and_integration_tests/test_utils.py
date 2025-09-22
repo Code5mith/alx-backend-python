c0ed67200d8ffc4b422b2063b605187fbbd64b36
Code5mith <jacobscontact1@gmail.com>
2025-09-21 19:27:57 -0700

    Task : Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import unittest
+from unittest.mock import patch
 from utils import *
 from parameterized import parameterized
 
@@ -8,7 +9,34 @@ class TestAccessNestedMap(unittest.TestCase):
         super().__init__(methodName)
 
     @parameterized.expand([
-    ({}, ("a",)),
-    ({"a":1}, ("a","b")),])
+    # Test case 1: simple key
+    ({"a": 1}, ("a",), 1),
+    # Test case 2: nested dictionary
+    ({"a": {"b": 2}}, ("a",), {"b": 2}),
+    # Test case 3: two-level nested key
+    ({"a": {"b": 2}}, ("a", "b"), 2),])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
     def test_access_nested_map_exception(self, nested_map, path):
-        self.assertRaises(access_nested_map(nested_map, path))
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_get.return_value.json.return_value = test_payload
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+        mock_get.assert_called_once_with(test_url)

2f015393cc1118c48aa1d2958c79784b29c013a0
Code5mith <jacobscontact1@gmail.com>
2025-10-05 18:49:18 -0400

    Fix : project

--- Django-Middleware-0x03/app/core/middleware.py
+++ Django-Middleware-0x03/app/core/middleware.py
@@ -1,5 +1,38 @@
 from django.http import HttpResponseForbidden
 
+
+import logging
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('requests.log')
+formatter = logging.Formatter('%(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
+class RequestLoggingMiddleware:
+    """
+    Middleware to log each user request to a file with timestamp, user, and path.
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Determine the user (Anonymous if not logged in)
+        user = request.user if request.user.is_authenticated else 'Anonymous'
+
+        # Log request
+        logger.info(f"{datetime.now()} - User: {user} - Path: {request.path}")
+
+        # Proceed to the next middleware/view
+        response = self.get_response(request)
+        return response
+
+
 class RolepermissionMiddleware:
     """
     Middleware to enforce role-based access.
